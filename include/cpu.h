4ebfbf87764cd7ddc407c9c2c6fe23a3f840bda3
Icaro Ramos <157264887+IcaroRamosDM@users.noreply.github.com>
2026-09-02 08:31:21 -0300

    feat: define initial CPU state

---
+++ include/cpu.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <stdbool.h>
+#include <stdint.h>
+
+enum
+{
+  CPU_MEMORY_SIZE = 256
+};
+
+typedef struct Cpu
+{
+  uint8_t register_a;
+  uint8_t register_b;
+  uint8_t program_counter;
+  bool zero_flag;
+  bool carry_flag;
+  bool halted;
+  uint64_t cycle_count;
+  uint8_t memory[CPU_MEMORY_SIZE];
+} Cpu;
--- src/main.c
+++ src/main.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 
+#include "cpu.h"
+
 int main(void)
 {
+  Cpu cpu = {0};
+
   puts("Virtual 8-bit microcontroller simulator");
+  printf("Memory size: %zu bytes\n", sizeof cpu.memory);
+
   return 0;
 }
 
